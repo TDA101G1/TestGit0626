f86140799725d408d350d6ce6f197bcf2a1d5314
sunset1117 <sunset1117@hotmail.com>
2020-06-26 16:52:54 +0800

    String name > StringBuffer name

--- src/com/model/AnimalVO.java
+++ src/com/model/AnimalVO.java
@@ -10,15 +10,13 @@ package com.model;
  * 產生新的類別繼承動物類別
  */
 public class AnimalVO {
-	private String name;
-	private int age;
+	private StringBuffer name;
 
 	private String test;
 	private Integer age;
 
 
 	
-12313123
 
 
 	
