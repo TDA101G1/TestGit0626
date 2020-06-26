7c1d22c98fb6ac3a95cee9f094f944715dda6e18
david7838 <forbid7838@gmail.com>
2020-06-26 16:10:09 +0800

    新增string方法

--- src/com/model/AnimalVO.java
+++ src/com/model/AnimalVO.java
@@ -2,16 +2,16 @@ package com.model;
 
 /*
  * 需要name、age、sex、sort；age改Integer
- * 
+ *
  * 產生get set方法
- * 
+ *
  * 產生吼叫的方法
- * 
+ *
  * 產生新的類別繼承動物類別
  */
 public class AnimalVO {
 	private String name;
 	private int age;
-	
-	
+	private String test;
+
 }
